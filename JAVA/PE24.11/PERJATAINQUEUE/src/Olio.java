287d192e910ebd6da1bdc7f8560bb1617d369f27
Kalle <kalhelm@hotmail.com>
2017-11-27 09:20:32 +0200

    jonoqueue ideointia

---
+++ JAVA/PE24.11/PERJATAINQUEUE/src/Olio.java
@@ -0,0 +1,25 @@
+public class Olio {
+    private String nimi;
+    private int id;
+
+    public Olio(String nimi, int id) {
+        this.nimi = nimi;
+        this.id = id;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ JAVA/PE24.11/PERJATAINQUEUE/src/QUEUE.java
@@ -0,0 +1,11 @@
+public class QUEUE {
+    Olio olio;
+    if queue is not empty {
+        step through queue {
+            if current.priority < priority {
+                insert element here
+                return
+            }
+        }
+    }
+}
