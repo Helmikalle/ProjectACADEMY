c9421bc5e16a308c16260cb1e06f5dc60569821e
Kalle <kalhelm@hotmail.com>
2017-11-30 10:28:19 +0200

    jotain pushia. (vanhojen tehtävien tutkimista)

--- JAVA/harjoitus1/src/Kertoma.java
+++ JAVA/harjoitus1/src/Kertoma.java
@@ -1,26 +1,35 @@
 
 import java.util.Scanner;
 
-public class Kertoma{
+public class Kertoma {
     public static void main(String[] args) {
 
-        long luku;
-        // halutaan sulkea Scanner, joten try with resources
-        try (Scanner in = new Scanner(System.in)) {
-            System.out.print("Anna luku, jolle kertoma lasketaan: ");
-            luku = in.nextLong();
-        } catch (Exception e) {
-            System.err.println("Ongelma lukemisessa: " + e.getMessage());
-            System.err.println("Lopetetaan..");
-            return;
+            long luku = 0;
+            // halutaan sulkea Scanner, joten try with resources
+            try (Scanner in = new Scanner(System.in)) {
+               for (;;){
+                System.out.print("Anna luku, jolle kertoma lasketaan. Nolla lopettaa: ");
+                luku = in.nextLong();
+                if(luku > 0){
+                System.out.println(String.format("%d! on: %d", luku, kertoma(luku)));
+                }else {
+                    System.out.println("Hei hei.");
+                    break;
+                }
+                }
+            } catch (Exception e) {
+                System.err.println("Ongelma lukemisessa: " + e.getMessage());
+                System.err.println("Lopetetaan..");
+                return;
+            }
+
         }
-        System.out.println(String.format("%d! on: %d", luku, kertoma(luku)));
-    }
+
 
     private static Object kertoma(long luku) {
-        long tulos=1;
-        for (int i = 1; i <=luku ; i++) {
-            tulos *=i;
+        long tulos = 1;
+        for (int i = 1; i <= luku; i++) {
+            tulos *= i;
 
         }
         return tulos;
