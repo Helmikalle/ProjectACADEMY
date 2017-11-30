2ba93c04f83c639277752dc9c8fd86f4cee6afc4
Kalle <kalhelm@hotmail.com>
2017-11-30 13:40:43 +0200

    pushia. vanhojen tehtävien tarkastelua

--- JAVA/Tiedostonlukija/src/Sanalaskuri.java
+++ JAVA/Tiedostonlukija/src/Sanalaskuri.java
@@ -2,15 +2,17 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Sanalaskuri {
-    public static void main(String[]args){
-        String tiedostonnimi;
-        if(args.length>0){
-            tiedostonnimi = args [0];
-        }else{
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        String tiedostonnimi = lukija.nextLine() ;
+        if (args.length > 0) {
+            tiedostonnimi = args[0];
+        } else {
             System.out.println("Oletus");
-            tiedostonnimi = "";
+
         }
         Sanalaskuri laskuri = new Sanalaskuri();//pääsee käsiksi non static
         laskuri.kasitteleTiedosto(tiedostonnimi);//samaa asiaa
@@ -18,16 +20,19 @@ public class Sanalaskuri {
     }
 
     private void kasitteleTiedosto(String nimi) {
-    int sanat=0, merkit=0, rivit=0;
-    String rivi;
+        int sanat = 0, merkit = 0, rivit = 0;
+        String rivi;
         try {
-            BufferedReader lukija=new BufferedReader(new FileReader(nimi));
-            for(rivi = lukija.readLine() ; rivi !=null; rivi = lukija.readLine());
-            rivit++;
-            merkit += rivi.length();
-            String [] sanatrivilla = rivi.split(" ");
-
-
+            BufferedReader lukija = new BufferedReader(new FileReader(nimi));
+            for (rivi = lukija.readLine(); rivi != null; rivi = lukija.readLine()) {
+                rivit++;
+                merkit += rivi.length() + 1;
+                String[] sanatrivilla = rivi.split("\\W+");
+                int sanoja = sanatrivilla.length;
+                sanat += sanoja;
+            }
+            System.out.println("------ " + nimi + " -------");
+            System.out.println("\t" + rivit + "\t" + sanat + "\t" + merkit);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
