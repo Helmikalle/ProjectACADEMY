796695e16a1441e14d219f8b4d12dc2a2bc83fa0
Kalle <kalhelm@hotmail.com>
2017-11-30 13:12:38 +0200

    pushia. vanhojen tehtävien tarkastelua

--- JAVA/Tiedostonlukija/src/Kirjoituskone.java
+++ JAVA/Tiedostonlukija/src/Kirjoituskone.java
@@ -3,10 +3,10 @@ import java.io.*;
 public class Kirjoituskone {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = null;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));//syötteen lukija
+        BufferedWriter writer; //syöttää tekstiä tiedostoon
         String tekstia = null;
-        File tiedosto = new File("moi.txt");
+        File tiedosto = new File("moi.txt"); //tiedoston nimi
 
         try {
             System.out.println(tiedosto.getCanonicalPath());
