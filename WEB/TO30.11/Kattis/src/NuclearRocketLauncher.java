8d8e6377994d8ea9660797dc2f456b24771edee2
Kalle <kalhelm@hotmail.com>
2017-12-29 14:57:59 +0200

    pfft

---
+++ WEB/TO30.11/Kattis/src/NuclearRocketLauncher.java
@@ -0,0 +1,6 @@
+public class NuclearRocketLauncher {
+
+    public static void main(String[] args) {
+
+    }
+}
--- WEB/TO30.11/RESTjaREACTkokeilu/src/main/java/fi/academy/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package fi.academy;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee,Long> {
-}
